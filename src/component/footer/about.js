e89ccc3b52fc9179cb7003ce1125298ccfd9b85c
abel-mendez <abel.m.villagra@gmail.com>
2021-10-07 21:33:13 -0300

    agregando validaciones

--- src/component/footer/about.js
+++ src/component/footer/about.js
@@ -3,10 +3,49 @@ import { useState } from "react";
 import "./about.css";
 const vacio = ({ email, asunto, consulta }) => {
   let formVacio = false;
-  if (email == "" || asunto == "" || consulta == "") {
+  if (email === "" || asunto === "" || consulta === "") {
     formVacio = true;
   }
   return formVacio;
+};
+const validar = (event, error) => {
+  let value = event.target.value;
+  let name = event.target.name;
+  if (value === "") {
+    error = {
+      ...error,
+      [name]: "Este campo no puede estar vacio",
+    };
+  } else if (name === "email" && !/\S+@\S+\.\S+/.test(value)) {
+    error = {
+      ...error,
+      [name]: "Debe ingresar un mail valido",
+    };
+  } else if (name === "asunto" && value.length < 10) {
+    error = {
+      ...error,
+      [name]: "Debe ingresar al menos 10 caracteres",
+    };
+  } else if (name === "consulta" && value.length > 256) {
+    error = {
+      ...error,
+      [name]: "Debe ingresar menos de 256 caracteres",
+    };
+  } else {
+    error = {
+      ...error,
+      [name]: "",
+    };
+  }
+  let errores = true;
+  if (error.email === "" && error.asunto === "" && error.consulta === "") {
+    errores = false;
+  }
+  error = {
+    ...error,
+    err: errores,
+  };
+  return error;
 };
 export default function About() {
   const [form, setForm] = useState({
@@ -27,11 +66,7 @@ export default function About() {
       [event.target.name]: event.target.value,
       vacio: vacio(form),
     });
-    setErrors({
-      ...errors,
-
-      vacio: vacio(form),
-    });
+    setErrors(validar(event, errors));
   };
   return (
     <div className="containerAbout">
@@ -52,6 +87,7 @@ export default function About() {
             name="email"
             className="input"
           />
+          {errors.email}
         </div>
         <div>
           <label for="asunto"> Asunto: </label>
@@ -62,6 +98,7 @@ export default function About() {
             name="asunto"
             className="input"
           />
+          {errors.asunto}
         </div>
         <label id="consulta">Tu consulta</label>
         <textarea
@@ -72,9 +109,11 @@ export default function About() {
           rows="10"
           cols="50"
         ></textarea>
+        {errors.consulta}
         <button className="button" disabled={errors.err || form.vacio}>
           Enviar
         </button>
+        {console.log(errors)}
       </form>
     </div>
   );
